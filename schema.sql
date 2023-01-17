add4741ce14c9a861fc0b0ab28c0cfde9db15770
anyepeter <petzyrockchendi@gmail.com>
2023-01-17 09:53:41 +0100

    create table animals

--- schema.sql
+++ schema.sql
@@ -1,7 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+id INT GENERATED ALWAYS AS IDENTIFY PRIMARY KEY,
+name CHAR NOT NULL,
+date_of_birth DATE NOT NULL,
+escape_attempts INT NOT NULL,
+neutered BOOLEAN,
+weight_kg DECIMAL(10,2)
 );
-
-CREATE DATABASE vet_clinic;
